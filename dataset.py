44ce371bde11cf71e3044103706ff09e4940e50a
Eugene Z <eugene.zabuzov@gmail.com>
2021-11-24 17:50:51 +0500

    Homework_point_2

--- dataset.py
+++ dataset.py
@@ -1,14 +1,58 @@
 import http.client
 import json
+from pprint import pprint
 
-
-RANDOM_DATA = False # Если True - данные каждый раз будут разными
+RANDOM_DATA = False  # Если True - данные каждый раз будут разными
 
 
 conn = http.client.HTTPSConnection('pumpskill.ru')
-conn.request('GET',f'/cases/api/python-basic/users-and-friends/{"?r=0" if not RANDOM_DATA else ""}')
+conn.request('GET', f'/cases/api/python-basic/users-and-friends/{"?r=0" if not RANDOM_DATA else ""}')
 response = conn.getresponse()
 data = json.loads(response.read().decode('utf-8'))
 
-users = data["users"]
+users = data['users']
 countries = data['countries']
+
+us = [{'mail': 'ljubov_41@mail.ru',
+  'name': 'Орест Ефстафьевич Зуев',
+  'password': '7589652',
+  'sex': 'M',
+  'username': 'galkinaolimpiada'},
+ {'friends': [{'job': {'occupation': 'Юрист', 'salary': 22131},
+               'mail': 'miron00@mail.ru',
+               'name': 'Евдокия Федоровна Харитонова',
+               'sex': 'F'},
+              {'cars': [{'Category': 'Pickup',
+                         'Make': 'Dodge',
+                         'Model': 'Ram 2500 Club Cab',
+                         'Year': 1999}],
+               'flights': [{'airport': 'Pierre-Elliott-Trudeau',
+                            'city': 'Dorval',
+                            'country': 'Canada'},
+                           {'airport': 'Cheju International airport',
+                            'city': 'Jeju-Si',
+                            'country': 'South Korea'},
+                           {'airport': 'New Incheon International airport',
+                            'city': 'Incheon',
+                            'country': 'South Korea'}],
+               'job': {'occupation': 'Нарколог', 'salary': 11321},
+               'mail': 'mefodigurev@hotmail.com',
+               'name': 'Зыкова София Иосифовна',
+               'sex': 'F'}],
+  'mail': 'efimovsamson@mail.ru',
+  'name': 'Королев Вадим Егорович',
+  'password': '2r805T7',
+  'sex': 'M',
+  'username': 'zikovaevpraksija'},
+ {'friends': [{'flights': [{'airport': 'Tokyo International airport',
+                            'city': 'Tokyo',
+                            'country': 'Japan'}],
+               'job': {'occupation': 'Упаковщик', 'salary': 18743},
+               'mail': 'xkalinina@hotmail.com',
+               'name': 'Кира Борисовна Щербакова',
+               'sex': 'F'}],
+  'mail': 'leonid85@yandex.ru',
+  'name': 'Быков Мина Еремеевич',
+  'password': '75Gq7',
+  'sex': 'M',
+  'username': 'budimir1976'}]
--- index.py
+++ index.py
@@ -1,19 +1,36 @@
 from dataset import users, countries
 from pprint import pprint
 
+
 pprint(users)
 # pprint(countries)
 
-# Вариант:
+# Вариант c алгоритмом:
 # users_wrong_passwords = []
 #
 # for user in users:
 #    if user['password'].isdigit():
 #        users_wrong_passwords.append({'name': user['name'], 'mail': user['mail']})
 
-users_wrong_passwords = [{'name': user['name'], 'mail': user['mail']} for user in users if user['password'].isdigit()]
-girls_drivers = [user['friends']['sex'] for user in users]  # FIXME
-pprint(girls_drivers)
-# print(users_wrong_passwords)
-# and user[1]['friends'][0]['cars']
-# if user[1]['friends'][0]['sex'] == 'F'
+# Генератор:
+# Формируем словарь по ключам имени и почты, если пароль пользователя состоит только из чисел
+# users_wrong_passwords = [{'name': user['name'], 'mail': user['mail']} for user in users
+#                          if user['password'].isdigit()]
+
+
+# Вариант с алгоритмом:
+# Единственное - есть вопрос: правильно ли я достал 'cars'? На первый взгляд все работает,
+# но если в списке будет больше двух словарей - тогда имя друга в выборку не попадет.
+# Другого способа как подобраться к 'cars' в т.ч. с использованием метода get() я не придумал #TODO
+# girls_drivers = []
+# for friend in users:
+#     if 'friends' in friend and friend['friends'][0]['sex'] == 'F' and 'cars' in friend['friends'][-1]:
+#         girls_drivers.append(friend.get('friends')[0].get('name'))
+
+# Генератор:
+# Получаем значение 'name' из полученного массива значений 'friends'
+# При условии, что 'friends' вообще есть в словаре, пол друга женский и друг владел машиной
+# girls_drivers = [friend.get('friends')[0].get('name') for friend in users
+#              if 'friends' in friend and friend['friends'][0]['sex'] == 'F' and 'cars' in friend['friends'][-1]]
+#
+# print(girls_drivers)
