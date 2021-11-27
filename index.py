8e24fe4f680665ddfc69b837fc339606313eee22
JLeruonep <eugene.zabuzov@gmail.com>
2021-11-27 09:34:43 +0500

    lst

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from dataset import users, countries
 from pprint import pprint
-
+users_copy = users
 
 # pprint(users)
 #   Point 1
@@ -9,7 +9,7 @@ from pprint import pprint
 # Вариант c алгоритмом:
 users_wrong_password = []
 
-for user in users:
+for user in users_copy:
     if user['password'].isdigit():
         users_wrong_password.append({'name': user['name'], 'mail': user['mail']})
 
@@ -26,7 +26,7 @@ for user in users:
 
 # Вариант с алгоритмом:
 girls_drivers = []
-for user in users:
+for user in users_copy:
     friends = user.get('friends', [])
     for friend in friends:
         if friend['sex'] == 'F' and friend.get('cars'):
@@ -45,7 +45,7 @@ max_salary = {
     'occupation': 'none',
     'salary': 0
 }
-for user in users:
+for user in users_copy:
     if 'friends' in user:  # Проверяем, чтобы в списке лежал словарь с ключом 'friends'
         if user['friends'][0]['job']['salary'] > max_salary['salary']:
             max_salary = {'occupation': user['friends'][0]['job']['occupation'],
@@ -57,7 +57,7 @@ sum_salaries = {
 
 }
 
-for user in users:
+for user in users_copy:
     if 'friends' in user:
         friends_salaries = 0
         for friend in user['friends']:
@@ -72,7 +72,7 @@ flights_count = 0
 
 # Для каждого пользователя смотрим, чтобы у него были друзья, а у друзей - машины.
 # Считаем количество друзей с машинами, считаем сколько было полетов у таких друзей
-for user in users:
+for user in users_copy:
     if 'friends' in user and 'cars' in user['friends'][-1]:
         friends_with_cars += 1
         if 'flights' in user['friends'][-1]:
@@ -81,7 +81,7 @@ avg_flights = round(flights_count / friends_with_cars, 5)
 
 #   Point 6
 
-for user in users:
+for user in users_copy:
     user_delete = False
     if 'friends' in user and 'flights' in user['friends'][-1]:
         for flight in user['friends'][-1]['flights']:
