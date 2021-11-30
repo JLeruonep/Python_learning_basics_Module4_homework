2c4fe7709d2a325c4cabb358c9c5f01bfd175083
Eugene Z <eugene.zabuzov@gmail.com>
2021-11-30 10:23:57 +0500

    Optimization

--- index.py
+++ index.py
@@ -1,19 +1,18 @@
 from dataset import users, countries
 from pprint import pprint
-friends = []
-for user in users:
-    friends = user.get('friends', [])
+
+
 #   Point 1
 
 # Вариант c алгоритмом:
 # users_wrong_password = []
-#
+
 # for user in users:
 #     if user['password'].isdigit():
 #         users_wrong_password.append({'name': user['name'], 'mail': user['mail']})
 
-# # Генератор:
-# # Формируем словарь по ключам имени и почты, если пароль пользователя состоит только из чисел
+# Генератор:
+# Формируем словарь по ключам имени и почты, если пароль пользователя состоит только из чисел
 users_wrong_password = [{'name': user['name'], 'mail': user['mail']} for user in users
                         if user['password'].isdigit()]
 
@@ -27,12 +26,10 @@ users_wrong_password = [{'name': user['name'], 'mail': user['mail']} for user in
 #         if friend['sex'] == 'F' and friend.get('cars'):
 #             girls_drivers.append(friend['name'])
 
-
 # Генератор:
 # Получаем значение 'name' из полученного массива значений 'friends'
 # При условии, что 'friends' вообще есть в словаре, пол друга женский и друг владел машиной
-
-girls_drivers = [friend['name'] for friend in friends
+girls_drivers = [friend['name'] for user in users for friend in user.get('friends', [])
                  if friend['sex'] == 'F' and friend.get('cars')]
 
 #   Point 3
@@ -41,11 +38,11 @@ best_occupation = {
     'occupation': 'none',
     'salary': 0
 }
-# for user in users:
-#     friends = user.get('friends', [])
-for friend in friends:
-    if friend['job']['salary'] > best_occupation['salary']:
-        best_occupation = {'occupation': friend['job']['occupation'], 'salary': friend['job']['salary']}
+for user in users:
+    friends = user.get('friends', [])
+    for friend in friends:
+        if friend['job']['salary'] > best_occupation['salary']:
+            best_occupation = {'occupation': friend['job']['occupation'], 'salary': friend['job']['salary']}
 
 #   Point 4
 
@@ -68,9 +65,12 @@ flights_count = 0
 
 # Для каждого пользователя смотрим, чтобы у него были друзья, а у друзей - машины.
 # Считаем количество друзей с машинами, считаем сколько было полетов у таких друзей
-# for user in users:
-#     friends = user.get('friends', [])
-#     for friend in friends:
+for user in users:
+    friends = user.get('friends', [])
+    for friend in friends:
+        if friend['cars'] and friend['flights']:
+            friends_with_cars += 1
+            flights_count += len(friend['flights'])
 
 # if 'friends' in user and 'cars' in user['friends'][-1]:
 #     friends_with_cars += 1
